2524ad7bd08fdc4ac6dbcaac0e5e4c330bd2c51c
adicomdotir <adicom.ir@gmail.com>
2023-04-21 22:30:06 +0330

    Add top header

--- app/src/main/java/ir/adicom/jedbizcard/MainActivity.kt
+++ app/src/main/java/ir/adicom/jedbizcard/MainActivity.kt
@@ -24,6 +24,8 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import ir.adicom.jedbizcard.ui.theme.JedBizCardTheme
 import androidx.compose.foundation.lazy.items
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 
 class MainActivity : ComponentActivity() {
@@ -32,11 +34,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             JedBizCardTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    CreateBizCard()
+                MyApp {
+                    TopHeader()
                 }
             }
         }
@@ -44,135 +43,42 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun CreateBizCard() {
-    val buttonClickedState = remember {
-        mutableStateOf(false)
+fun MyApp(content: @Composable () -> Unit) {
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colors.background
+    ) {
+        content()
     }
+}
+
+@Preview
+@Composable
+fun TopHeader(totalPerPerson: Double = 0.0) {
     Surface(
         modifier = Modifier
             .fillMaxWidth()
-            .fillMaxHeight()
+            .height(150.dp)
+            .clip(
+                shape = RoundedCornerShape(
+                    corner = CornerSize(12.dp)
+                )
+            ),
+        color = Color(0xFFE9D7F7)
     ) {
-        Card(
-            modifier = Modifier
-                .width(200.dp)
-                .height(390.dp)
-                .padding(12.dp),
-            shape = RoundedCornerShape(corner = CornerSize(15.dp)),
-            backgroundColor = Color.White,
-            elevation = 4.dp
+        Column(
+            modifier = Modifier.padding(12.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
         ) {
-            Column(
-                modifier = Modifier.height(300.dp),
-                verticalArrangement = Arrangement.Top,
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                CreateImageProfile()
-                Divider()
-                ProfileInfo()
-                Button(onClick = {
-                    buttonClickedState.value = !buttonClickedState.value
-                }) {
-                    Text(text = "Portfolio", style = MaterialTheme.typography.button)
-                }
-                if (buttonClickedState.value) {
-                    Content()
-                } else {
-                    Box {
-
-                    }
-                }
-            }
-
+            val total = "%.2f".format(totalPerPerson)
+            Text(text = "Total Per Person", style = MaterialTheme.typography.h5)
+            Text(
+                text = "$$total",
+                style = MaterialTheme.typography.h4,
+                fontWeight = FontWeight.Bold
+            )
         }
-    }
-}
-
-@Composable
-fun Content() {
-    Box(
-        modifier = Modifier
-            .fillMaxHeight()
-            .fillMaxWidth()
-            .padding(5.dp)
-    ) {
-        Surface(
-            modifier = Modifier
-                .fillMaxHeight()
-                .fillMaxWidth()
-                .padding(3.dp),
-            shape = RoundedCornerShape(corner = CornerSize(6.dp)),
-            border = BorderStroke(2.dp, color = Color.LightGray)
-        ) {
-            Portfolio(data = listOf("Project 1", "Project 2", "Project 3", "Project 4"))
-        }
-    }
-}
-
-@Composable
-fun Portfolio(data: List<String>) {
-    LazyColumn {
-        items(data) { item ->
-            Card(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(13.dp),
-                shape = RoundedCornerShape(corner = CornerSize(6.dp)),
-                elevation = 4.dp
-            ) {
-                Row(
-                    modifier = Modifier
-                        .padding(8.dp)
-                        .background(MaterialTheme.colors.surface)
-                        .padding(16.dp)
-                ) {
-                    CreateImageProfile(modifier = Modifier.size(100.dp))
-                    Column(
-                        modifier = Modifier
-                            .padding(7.dp)
-                            .align(alignment = Alignment.CenterVertically)
-                    ) {
-                        Text(text = item, fontWeight = FontWeight.Bold)
-                        Text(text = "A great Project", style = MaterialTheme.typography.body2)
-                    }
-                }
-            }
-        }
-    }
-}
-
-@Composable
-private fun ProfileInfo() {
-    Column(modifier = Modifier.padding(5.dp)) {
-        Text(
-            "David B.",
-            style = MaterialTheme.typography.h4,
-            color = MaterialTheme.colors.primaryVariant
-        )
-        Text("Android Compose Programmer", modifier = Modifier.padding(3.dp))
-        Text(
-            "@davidCompose", modifier = Modifier.padding(3.dp),
-            style = MaterialTheme.typography.subtitle1
-        )
-    }
-}
-
-@Composable
-private fun CreateImageProfile(modifier: Modifier = Modifier) {
-    Surface(
-        modifier = modifier
-            .size(150.dp)
-            .padding(5.dp),
-        shape = CircleShape,
-        border = BorderStroke(0.5.dp, color = Color.LightGray),
-        elevation = 4.dp,
-        color = MaterialTheme.colors.onSurface.copy(alpha = 0.5f)
-    ) {
-        Image(
-            painter = painterResource(id = R.drawable.profile_image),
-            contentDescription = "profile image",
-            contentScale = ContentScale.Crop
-        )
     }
 }
 
@@ -180,6 +86,8 @@ private fun CreateImageProfile(modifier: Modifier = Modifier) {
 @Composable
 fun DefaultPreview() {
     JedBizCardTheme {
-        CreateBizCard()
+        MyApp {
+            TopHeader()
+        }
     }
 }
