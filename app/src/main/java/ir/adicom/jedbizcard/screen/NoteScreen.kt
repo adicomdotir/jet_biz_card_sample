8bc6fd4d7eec1d58c226255a3aecd4fd19267238
adicomdotir <adicom.ir@gmail.com>
2023-04-29 23:53:56 +0330

    Add note screen with new structure for note app

---
+++ app/src/main/java/ir/adicom/jedbizcard/screen/NoteScreen.kt
@@ -0,0 +1,42 @@
+package ir.adicom.jedbizcard.screen
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Icon
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Notifications
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import ir.adicom.jedbizcard.R
+
+@Composable
+fun NoteScreen() {
+    Column(modifier = Modifier.padding(6.dp)) {
+        TopAppBar(
+            title = {
+                Text(text = stringResource(id = R.string.app_name))
+            },
+            actions = {
+                Icon(
+                    imageVector = Icons.Rounded.Notifications,
+                    contentDescription = "Notifications"
+                )
+            },
+            backgroundColor = Color(0xFFDADFE3)
+        )
+
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+
+        }
+    }
+}
