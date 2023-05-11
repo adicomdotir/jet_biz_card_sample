03a3add9d4828db4f96169816ae17350b519b647
adicomdotir <adicom.ir@gmail.com>
2023-05-11 23:43:09 +0330

    Add animation for splash with navigation

--- app/src/main/java/ir/adicom/jedbizcard/navigation/WeatherNavigation.kt
+++ app/src/main/java/ir/adicom/jedbizcard/navigation/WeatherNavigation.kt
@@ -4,7 +4,8 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import ir.adicom.jedbizcard.screens.WeatherSplashScreen
+import ir.adicom.jedbizcard.screens.main.MainScreen
+import ir.adicom.jedbizcard.screens.splash.WeatherSplashScreen
 
 @Composable
 fun WeatherNavigation() {
@@ -12,6 +13,9 @@ fun WeatherNavigation() {
     NavHost(navController = navController, startDestination = WeatherScreens.SearchScreen.name) {
         composable(WeatherScreens.SearchScreen.name) {
             WeatherSplashScreen(navController = navController)
+        }
+        composable(WeatherScreens.MainScreen.name) {
+            MainScreen(navController = navController)
         }
     }
 }
