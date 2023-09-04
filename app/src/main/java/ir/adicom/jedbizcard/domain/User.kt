217e282b8a13620460c02d637f22bea4d20eef2a
adicomdotir <adicom.ir@gmail.com>
2023-09-04 22:01:43 +0330

    Add tic tac toe sample

--- app/src/main/java/ir/adicom/jedbizcard/domain/User.kt
+++
@@ -1,7 +0,0 @@
-package ir.adicom.jedbizcard.domain
-
-data class User(
-    val id: Long,
-    val name: String,
-    val email: String,
-)
--- app/src/main/java/ir/adicom/jedbizcard/domain/UserRepository.kt
+++
@@ -1,5 +0,0 @@
-package ir.adicom.jedbizcard.domain
-
-interface UserRepository {
-    suspend fun getUser(id: Long): User?
-}
