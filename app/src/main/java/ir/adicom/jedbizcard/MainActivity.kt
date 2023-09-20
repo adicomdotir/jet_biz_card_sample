053382054f21c753a53f6bd7a40377164e8db7e1
adicomdotir <adicom.ir@gmail.com>
2023-09-20 22:56:02 +0330

    Add switch & checkbox sample

--- app/src/main/java/ir/adicom/jedbizcard/MainActivity.kt
+++ app/src/main/java/ir/adicom/jedbizcard/MainActivity.kt
@@ -1,32 +1,31 @@
 package ir.adicom.jedbizcard
 
 import android.os.Bundle
-import android.util.Log
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.animation.core.Spring
-import androidx.compose.animation.core.animateDpAsState
-import androidx.compose.animation.core.spring
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.selection.selectable
+import androidx.compose.material.Checkbox
+import androidx.compose.material.RadioButton
+import androidx.compose.material.Switch
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.TextFieldValue
-import androidx.compose.ui.text.toUpperCase
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import dagger.hilt.android.AndroidEntryPoint
-import java.util.*
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
@@ -47,7 +46,24 @@ fun DefaultPreview() {
 
 @Composable
 private fun MyApp(modifier: Modifier = Modifier) {
+    var switchValue by remember {
+        mutableStateOf(false)
+    }
+    var checkboxValue by remember {
+        mutableStateOf(false)
+    }
+
     Column {
-        RadioButton(selected = true, onClick = { /*TODO*/ })
+        Switch(
+            checked = switchValue,
+            onCheckedChange = {
+                switchValue = it
+            }
+        )
+        Checkbox(
+            checked = checkboxValue, onCheckedChange = {
+                checkboxValue = it
+            }
+        )
     }
 }
